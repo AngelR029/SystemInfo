3fc3c663733a38dc3e3b69da433d267b2d9f61fe
angel <angel.rodriguez029@mymdc.net>
2016-02-06 14:07:57 -0500

    Done

---
+++ VarInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarInt
+{
+    class Program
+    {
+
+        
+        public static void PrintLargestDivisibleBy5(params int[] list)
+        {
+
+            int largest = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((list[i] >= largest) && (list[i] % 5 == 0))
+                {
+                    largest = list[i];
+                }
+            }
+            Console.WriteLine(largest);           
+        }
+        static void Main(string[] args)
+        {
+            PrintLargestDivisibleBy5(5,5,10,22,50,0);
+            Console.ReadLine();
+        }
+    }
+}
